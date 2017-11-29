7ba5b09a10fb0509735a35973e8f797dd919e7ca
meiyaoyao <495901421@qq.com>
2017-11-29 22:58:15 +0800

    首页完成

--- public/admin/js/common.js
+++ public/admin/js/common.js
@@ -14,3 +14,31 @@ $(document).ajaxStop(function () {
         NProgress.done();
     }, 500);
 });
+
+
+// 二级菜单隐藏显示功能
+$(".child").prev().on("click",function(){
+$(this).next().slideToggle();
+});
+// 侧边栏显示隐藏功能
+$(".icon_menu").on("click",function(){
+    $(".lt-aside").toggleClass("now");
+    $(".lt-main").toggleClass("now");
+    $(".topbar").toggleClass("now");
+});
+//退出功能
+$(".icon_logout").on("click",function(){
+    $("#logoutmodal").modal("show");
+    $(".btn_logout").off().on("click",function(){
+        $.ajax({
+            type:"get",
+            url:"/employee/employeeLogout",
+            success:function(data){
+                if (data.success) {
+                    //退出成功
+                    location.href = "login.html";
+                }
+            }
+        });
+    });
+});
